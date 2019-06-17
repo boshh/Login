6a1a34041408a868faab3242e03e771c69242477
bosh <bosh1208@gmail.com>
2019-06-18 02:19:22 +0800

    Updates

---
+++ precache-manifest.384eb8a9f98c2945b1c7367736466327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d9d18d9d3a70f99973c8b86f33e9b4",
+    "url": "/reactLogin/index.html"
+  },
+  {
+    "revision": "fca8ced34dd71fdeeb76",
+    "url": "/reactLogin/static/css/main.b26028e7.chunk.css"
+  },
+  {
+    "revision": "89a65aadfd20c327f48c",
+    "url": "/reactLogin/static/js/2.fc729d1c.chunk.js"
+  },
+  {
+    "revision": "fca8ced34dd71fdeeb76",
+    "url": "/reactLogin/static/js/main.91e1acaf.chunk.js"
+  },
+  {
+    "revision": "c4f5e7736be2772507a5",
+    "url": "/reactLogin/static/js/runtime~main.08fff344.js"
+  }
+]);
